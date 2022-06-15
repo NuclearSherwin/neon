ac683631c8647fe9318d4f6dd91dc457249b29c9
Phong Mai <phongspacenasa@gmail.com>
2022-06-15 10:24:57 +0700

    success display information from post table to the view

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
 {
+    private $em;
+    private $postRepository;
+
+    //constructor will execute when objects created
+    public function __construct(EntityManagerInterface $em, PostRepository $postRepository)
+    {
+        $this->em = $em;
+        $this->postRepository = $postRepository;
+    }
+
+
     /**
      * @Route("/home/posts", name="neon_posts")
      */
     public function index(): Response
     {
+
+        $posts = $this->postRepository->findAll();
+
         return $this->render('posts/index.html.twig', [
-            'controller_name' => 'PostsController',
+            'posts' => $posts,
         ]);
     }
 }
