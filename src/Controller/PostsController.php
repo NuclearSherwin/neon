7ff3d1a7518b6813c4f536d7309ec516db94485f
Phong Mai <phongspacenasa@gmail.com>
2022-06-15 21:42:08 +0700

    success detail for post

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Entity\User;
+use App\Form\PostFormType;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,4 +38,55 @@ class PostsController extends AbstractController
             'posts' => $posts,
         ]);
     }
+
+
+
+    /**
+     * @Route("/home/posts/create", name="create_posts", methods={"GET", "POST"})
+     */
+    public function createPost(Request $request): Response
+    {
+        //create new object
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+
+        $form->handleRequest($request);
+
+        //validation submit for post
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newPost = $form->getData();
+            $imgPath = $form->get('imgPath')->getData();
+            if ($imgPath) {
+                // identifier the dot at the end of the picture
+                $newFileName = uniqid() . '.' . $imgPath->guessExtension();
+
+                try {
+                    $imgPath->move(
+                    //kernel will set up bundles used by the application and provide them with app's configurations
+                        $this->getParameter('kernel.project_dir') . '/public/uploads',
+                        $newFileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+
+                }
+
+                $newPost->setImgPath('/uploads/' . $newFileName);
+            }
+
+                //save the path of img into your project (/public/uploads/)
+                $this->em->persist($newPost);
+                $this->em->flush();
+
+                return $this->redirectToRoute('neon_posts');
+        }
+
+
+        return $this->render('posts/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
 }
+
+
--- src/Form/PostFormType.php
+++ src/Form/PostFormType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Post;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,20 @@ class PostFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description')
-            ->add('createAt')
-            ->add('imgPath')
+            ->add('description', TextareaType::class, [
+                'attr' => array(
+                    'placeholder' => 'What on your mind to day?'
+                ),
+                'label' => false
+            ])
+            ->add('imgPath', FileType::class, [
+                'label' => false,
+                'required' => false,
+                'mapped' => false,
+            ])
+            ->add('createAt', DateTimeType::class, [
+                'input' => 'datetime_immutable',
+            ])
             ->add('user')
         ;
     }
