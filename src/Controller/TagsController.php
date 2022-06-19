6a842a6c2656d42489b95689b68b2892d1f6e2bb
Demon Master <you@example.com>
2022-06-19 09:11:21 +0700

    Tagcontroller ,display data for tags,view,entity for tag

--- src/Controller/TagsController.php
+++ src/Controller/TagsController.php
@@ -2,19 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Tag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TagsController extends AbstractController
 {
+//    private $em;
+//    private $postResponsitory;
+//    public function __construct(EntityManagerInterface $em, TagRepository $tagRepository)
+//    {
+//        $this->em = $em;
+//        $this->TagRepository = $tagRepository;
+//    }
+
+
+//    /**
+//     * @Route("/tags", name="app_tags")
+//     */
+//    public function index(): Response
+//    {
+//        return $this->render('tags/index.html.twig', [
+//            'controller_name' => 'TagsController',
+//        ]);
+//    }
+
+
     /**
-     * @Route("/tags", name="app_tags")
+     * @Route("home/tags", name="app_tags")
      */
-    public function index(): Response
+    public function listNews(): Response
     {
+
+        $tags = $this->getDoctrine()->getManager()->getRepository(Tag::class)
+            ->findAll();
+
         return $this->render('tags/index.html.twig', [
-            'controller_name' => 'TagsController',
+            'tags' => $tags,
         ]);
     }
 }
