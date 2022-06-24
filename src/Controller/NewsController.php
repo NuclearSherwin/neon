b0bc94fb8adf78b81a69b942575dbc8a9555e2e9
Trong-khanh <tltkhanh1501@gmail.com>
2022-06-24 16:47:20 +0700

    edit create

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -9,13 +9,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+
 
 class NewsController extends AbstractController
 {
 
     /**
-     * @Route("home/news", name="neon_news")
+     * @Route("/news", name="neon_news")
      */
     public function listNews(): Response
     {
@@ -32,7 +32,7 @@ class NewsController extends AbstractController
     // show detail of news
 
     /**
-     * @Route("/home/news/detail/{id}", name="detail_news", methods={"GET"})
+     * @Route("/news/detail/{id}", name="detail_news", methods={"GET"})
      */
     public function showDetail($id)
     {
@@ -44,10 +44,10 @@ class NewsController extends AbstractController
         ]);
     }
 
-    // create newws function
+    // create news function
 
     /**
-     * @Route("/home/news/create", name="news_create", methods={"GET","POST"})
+     * @Route("/news/create", name="news_create", methods={"GET","POST"})
      */
     public function create(Request $request)
     {
@@ -55,26 +55,65 @@ class NewsController extends AbstractController
         $form = $this->createForm(NewsType::class, $Cnews);
         $form->handleRequest($request);
         // if press submit putting data to database
-        if ($form->isSubmitted() && $form->isValid()) {
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->render('news/create.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+
+        $News = $form->getData();
+        $imgpath = $form->get('imgpath')->getData();
+
+        if (!$imgpath) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($Cnews);
             $em->flush();
 
-            $this-> addFlash('notice','Submitted Successfully ');
+            $this->addFlash('notice', 'Submitted Successfully ');
 
             //back to news page then create a news successfully
             return $this->redirectToRoute('neon_news');
         }
-        return $this->render('news/create.html.twig', [
-            'form' => $form->createView()
-        ]);
+
+        $newFile = uniqid() . '.' . $imgpath->guessExtension();
+
+        try {
+            $imgpath->move(
+
+                $this->getParameter('kernel.project_dir') . '/public/news',
+                $newFile
+            );
+        } catch (FileException $e) {
+            return new Response($e->getMessage());
+
+        }
+
+        $News->setImgPath('/news/' . setImgPath);
+
+
+        $em = $this->getDoctrine()->getManager();
+
+
+        $em->persist($Cnews);
+        $em->flush();
+
+        $this->addFlash('notice', 'Submitted Successfully ');
+
+        //back to news page then create a news successfully
+        return $this->redirectToRoute('neon_news');
+
+
     }
 
-    //Update function
+
+
+//Update function
+
     /**
-     * @Route("/home/news/update/{id}", name="news_update", methods={"GET","POST"})
+     * @Route("/news/update/{id}", name="news_update", methods={"GET","POST"})
      */
-    public function update($id, Request $request): Response
+    public
+    function update($id, Request $request): Response
     {
         $Dnews = $this->getDoctrine()->getRepository(News::class)->find($id);
         $form = $this->createForm(NewsType::class, $Dnews);
@@ -86,7 +125,7 @@ class NewsController extends AbstractController
             $em->persist($Dnews);
             $em->flush();
 
-            $this-> addFlash('notice','update Successfully ');
+            $this->addFlash('notice', 'update Successfully ');
 
             //back to news page then update a new successfully
             return $this->redirectToRoute('neon_news');
@@ -96,18 +135,20 @@ class NewsController extends AbstractController
         ]);
     }
 
-    //delete function
+//delete function
 
     /**
      * @Route("/home/news/delete/{id}", name="news_delete", methods={"GET"})
      */
-    public function delete($id){
+    public
+    function delete($id)
+    {
         $Denews = $this->getDoctrine()->getRepository(News::class)->find($id);
         $em = $this->getDoctrine()->getManager();
         $em->remove($Denews);
         $em->flush();
 
-        $this->addFlash('notice','Delete Successfully ');
+        $this->addFlash('notice', 'Delete Successfully ');
         return $this->redirectToRoute('neon_news');
     }
 }
