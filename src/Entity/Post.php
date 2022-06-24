450b6f406f647a0fdd140a8a09522416671a8469
Phong Mai <phongspacenasa@gmail.com>
2022-06-24 15:33:17 +0700

    chaging relationship between post and tag to ManyToOne

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -39,7 +39,7 @@ class Post
     private $user;
 
     /**
-     * @ORM\OneToOne(targetEntity=Tag::class, cascade={"persist", "remove"})
+     * @ORM\ManyToOne(targetEntity=Tag::class, cascade={"persist", "remove"})
      * @ORM\JoinColumn(nullable=false)
      */
     private $tag;
