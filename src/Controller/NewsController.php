8057b4ef123b3b53244047e46cede933c4556744
Trong-khanh <tltkhanh1501@gmail.com>
2022-06-28 15:24:30 +0700

    commit to change branch

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -7,6 +7,7 @@ use App\Form\NewsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class NewsController extends AbstractController
 {
